d76f8dad113278fb00e885e65071610d497ebc52
Evan Tan <tcyevan@gmail.com>
2020-04-08 19:36:36 +1000

    Added calculations

--- sensors/README.md
+++ sensors/README.md
@@ -3,3 +3,13 @@ Currently contains code for blob statistics.
 
 # How to use:
 In sensors/build run `cmake ..` then `make`.
+
+# Resources used
+## blob_statistics
+https://docs.opencv.org/3.4/d0/d7a/classcv_1_1SimpleBlobDetector.html
+https://docs.microsoft.com/en-us/cpp/cpp/constructors-cpp?view=vs-2019
+https://stackoverflow.com/questions/7643342/convert-keypoints-to-mat-or-save-them-to-text-file-opencv
+https://stackoverflow.com/questions/10167534/how-to-find-out-what-type-of-a-mat-object-is-with-mattype-in-opencv
+https://stackoverflow.com/questions/25642532/opencv-pointx-y-represent-column-row-or-row-column
+https://stackoverflow.com/questions/21613246/what-is-the-structure-of-point2f-in-opencv
+https://stackoverflow.com/questions/42802294/c-opencv-iterate-through-pixels-in-a-mat-which-is-roi-of-another-mat
--- sensors/src/blob_statistics.cpp
+++ sensors/src/blob_statistics.cpp
@@ -3,106 +3,113 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/opencv.hpp>
+#include <cmath>
 
 int main(int argc, char *argv[])
 {
+    // Begin video capture
     cv::VideoCapture camera(0);
-    cv::Mat image_original, image_processed;
-    cv::Mat image_blob, image_hsv;
-
     std::cout << "Blob Statistics\nPress 'q' to quit\n";
 
+    // Create windows
     cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
     cv::namedWindow("Processed", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Drawing", cv::WINDOW_AUTOSIZE);
 
-    cv::SimpleBlobDetector::Params params;
-    params.minThreshold = 150;          // oh shit
-    params.maxThreshold = 500;
-    params.filterByArea = false;        // Filter by Area.
-    params.minArea = 500;
-    params.filterByCircularity = true;  // Filter by Circularity
-    params.minCircularity = 0.1;
-    params.filterByConvexity = true;    // Filter by Convexity
-    params.minConvexity = 0.5;
-    params.filterByInertia = false;     // Filter by Inertia
-    params.minInertiaRatio = 0.01;
-    params.blobColor = 0;
-    std::vector<cv::KeyPoint> keypoints;    // Storage for blobs
-    cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params); // Set up detector with params
+    // Create image containers
+    cv::Mat image_original, image_processed, image_drawing;
 
-    int erosion_dilation_size = 1;
-    cv::Mat element_erosion = getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosion_dilation_size + 1, 2 * erosion_dilation_size + 1), cv::Point(erosion_dilation_size, erosion_dilation_size)); 
-    cv::Mat element_dilation = getStructuringElement(cv::MORPH_RECT, cv::Size(2 * erosion_dilation_size + 1, 2 * erosion_dilation_size + 1), cv::Point(erosion_dilation_size, erosion_dilation_size));
+    // Set to 2 d.p. though not necessary
+    std::cout << std::setprecision(2) << std::fixed;
 
     while (true)
     {
-
         // Part 1
+        // Continuously grab image from camera
         camera >> image_original;
         if (image_original.empty())
         {
-            
             std::cout << "Could not access camera...\n";
             break;
         }
 
         // Part 3
-        image_hsv = cv::Mat::zeros(cv::Size(image_original.rows, image_original.cols), CV_8UC3);
-        cv::cvtColor(image_original, image_hsv, cv::COLOR_BGR2HSV);
-        cv::inRange(image_hsv, cv::Scalar(20, 80, 130), cv::Scalar(50, 225, 225), image_processed);
-        
-        std::vector<cv::Mat> hsv_channels;
-        hsv_channels.clear();
-        hsv_channels.resize(image_processed.channels());
-        cv::split(image_processed, &hsv_channels[0]);
-        image_blob = hsv_channels[2];
-            
+        // Convert image from BGR colorspace to grayscale
+        cv::cvtColor(image_original, image_processed, cv::COLOR_BGR2GRAY);
         // Part 4
-        // Remove background noise by eroding and dilating
-        cv::erode(image_blob, image_blob, element_erosion);
-        cv::dilate(image_blob, image_blob, element_dilation);
-        detector->detect(image_blob, keypoints);
-        drawKeypoints(image_blob, keypoints, image_blob, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        // Conert image from grayscale to binary
+        cv::threshold(image_processed, image_processed, 100, 255, cv::THRESH_BINARY);
 
-        int m00;
-
-        // Part 6
-        for (int i = 0; i < image_blob.rows; i++)
+        // Part 6 & 7
+        // Calculations
+        float m00, m10, m01, m20, m02, m11, theta;
+        m00 = 0.f, m10 = 0.f, m01 = 0.f, m20 = 0.f, m02 = 0.f, m11 = 0.f, theta = 0.f;
+        for (int i = 0; i < image_processed.rows; i++)
         {
-            for (int j = 0; j < image_blob.cols; j++)
+            for (int j = 0; j < image_processed.cols; j++)
             {
-                // std::cout << image_blob.at<unsigned char>(i,j);
+                if (image_processed.at<unsigned char>(i, j) == 0)
+                {
+                    m00++;
+                    m10 += i;
+                    m01 += j;
+                    m20 += pow(i, 2);
+                    m02 += pow(j, 2);
+                    m11 += i * j;
+                }
             }
         }
+        
+        theta = 0.5 * atan2(2 * (m00 * m11 - m10 * m01), (m00 * m20 - pow(m10, 2) - (m00 * m02 - pow(m01, 2))));
 
+        std::cout << "\n";
+        std::cout << "m00: " << m00 << std::setprecision(2) << "\n";
+        std::cout << "m10: " << m10 << std::setprecision(2) << "\t";
+        std::cout << "m01: " << m01 << std::setprecision(2) << "\n";
+        std::cout << "m20: " << m20 << std::setprecision(2) << "\t";
+        std::cout << "m02: " << m02 << std::setprecision(2) << "\n";
+        std::cout << "\n";
 
+        // Coordinate of centre of area
+        cv::Point2f center(m01 / m00, m10 / m00);
+
+        // Drawing functions
+        // Convert CV_8UC1 to CV_8UC3 image so we can draw on image
+        cv::cvtColor(image_processed, image_drawing, cv::COLOR_GRAY2BGR);
+
+        // Draw stuff
+        cv::circle(image_original, center, 20, cv::Scalar(0, 0, 255), 2);
+        cv::circle(image_processed, center, 20, cv::Scalar(255, 255, 255), 2);
+        cv::circle(image_drawing, center, 5, cv::Scalar(0, 0, 255), 10);
+
+        // Draw cross for centre
+        cv::line(image_drawing, cv::Point2f(center.x, center.y + 50), 
+                                cv::Point2f(center.x, center.y - 50), 
+                                cv::Scalar(0, 0, 255), 1);
+        cv::line(image_drawing, cv::Point2f(center.x + 50, center.y), 
+                                cv::Point2f(center.x - 50, center.y), 
+                                cv::Scalar(0, 0, 255), 1);
+        
+        // Draw line for axis of rotation
+        for (int k = 0; k < 25; k++)
+        {
+            cv::line(image_drawing, cv::Point2f(center.x + k * std::sin(theta), center.y + k * std::cos(theta)), 
+                                    cv::Point2f(center.x - k * std::sin(theta), center.y - k * std::cos(theta)), 
+                                    cv::Scalar(0, 255, 0), 1);
+        }
+        
+        // Display image on window
         cv::imshow("Original", image_original);
         cv::imshow("Processed", image_processed);
+        cv::imshow("Drawing", image_drawing);
 
         // Part 2
+        // Exit program via 'q'
         char key = (char)cv::waitKey(30);
         if (key == 'q' || key == 27)
         {
-            // key == 27 for ESC
             break;
         }
     }
     return 0;
 }
-
-// // Convert half of the image to gray
-// for (int y = 0; y < frame->height / 2; y++) {
-//   for (int x = 0; x < frame->width; x++) {
-//     // This is a pointer to the start of the current row.
-//     //  Note: The image is stored as a 1-D array which is mapped back
-//     //  into 2-space by multiplying the widthStep (the image width rounded
-//     //  to a "nice" value, eg a multiple of 4 or 8 depending on the OS and
-//     //  CPU) by the row number.
-//     uchar *row = (uchar *)(frame->imageData + frame->widthStep * y);
-
-//     int gray = (row[x * 3] + row[x * 3 + 1] + row[x * 3 + 2]) / 3;
-
-//     row[x * 3] = gray;
-//     row[x * 3 + 1] = gray;
-//     row[x * 3 + 2] = gray;
-//   }
